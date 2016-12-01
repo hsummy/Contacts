70ea7f4c2fd0944be0f52d8d9a38e8d1e7383d59
Heather Summy <hsummy@hotmail.com>
2016-12-01 13:34:52 -0500

    Initial Commit

---
+++ Contacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts
+//
+//  Created by HSummy on 12/1/16.
+//  Copyright © 2016 HSummy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Contacts/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Contacts
+//
+//  Created by HSummy on 12/1/16.
+//  Copyright © 2016 HSummy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
